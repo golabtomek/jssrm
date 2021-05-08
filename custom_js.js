a711aac90a1b1256f23f2ec41e3f269214e2256f
Tomasz Gołąb <golabtomek@gmail.com>
2021-05-08 15:54:18 +0200

    Update custom_js.js

--- custom_js.js
+++ custom_js.js
@@ -2,6 +2,6 @@ jQuery(document).ready(function($) {
  $(function () {
         $("body > div.uk-container.uk-container-center.uk-margin-top.uk-margin-large-bottom > nav > ul > li:nth-child(1) > a").html("Session Results");
         $("body > div.uk-container.uk-container-center.uk-margin-top.uk-margin-large-bottom > nav > ul > li:nth-child(2) > a").html("Leaderboards");
-        $("body > div.uk-container.uk-container-center.uk-margin-top.uk-margin-large-bottom > nav > ul > li:nth-child(3) > a").html("Series");
+        $("body > div.uk-container.uk-container-center.uk-margin-top.uk-margin-large-bottom > nav > ul > li:nth-child(3) > a").html("Championships");
     });
 });
