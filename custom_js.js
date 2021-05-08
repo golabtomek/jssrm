ff1af66509212afdc2576c653d41bd5898afd631
Tomasz Gołąb <golabtomek@gmail.com>
2021-05-08 15:33:04 +0200

    Update custom_js.js

--- custom_js.js
+++ custom_js.js
@@ -1,1 +1,5 @@
-document.querySelector("body > div.uk-container.uk-container-center.uk-margin-top.uk-margin-large-bottom > nav > ul > li:nth-child(1) > a").innerHTML = "Session Results";
+jQuery(document).ready(function($) {
+ $(function () {
+        $("body > div.uk-container.uk-container-center.uk-margin-top.uk-margin-large-bottom > nav > ul > li:nth-child(1) > a").html("Session Results")
+    });
+});
